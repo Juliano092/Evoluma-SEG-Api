b1ec1675addbb651f8e11074b2b720b2b41244de
Juliano <julianocardoso2611@gmail.com>
2025-11-26 11:24:20 -0300

    Revert "Primeiro commit do projeto"
    This reverts commit ee2b79d8e337f8a56013b6f03e5a95b5b9dd95eb.

--- src/models/Posto.js
+++
@@ -1,9 +0,0 @@
-const { DataTypes } = require('sequelize');
-const sequelize = require('../config/database');
-
-const Posto = sequelize.define('Posto', {
-    codigo: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
-    nome_fantasia: { type: DataTypes.STRING } // Importante para o relatório
-}, { tableName: 'posto', timestamps: false });
-
-module.exports = Posto;
